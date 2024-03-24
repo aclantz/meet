d448fa3eab94fb5722b074dca29994cce7b8067c
Alex Lantz <aclantz321@gmail.com>
2024-03-24 16:37:47 -0400

    add getAccessToken func and test-site, exercise4.2

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -20,6 +20,7 @@ const oAuth2Client = new google.auth.OAuth2(
 
 //Functions
 module.exports.getAuthURL = async () => {
+  //allows for seamless retrieval, refresh, and retry of request for auth URL
   const authUrl = oAuth2Client.generateAuthUrl({
     access_type: "offline",
     scope: SCOPES,
@@ -37,3 +38,33 @@ module.exports.getAuthURL = async () => {
   };
 };
 
+module.exports.getAccessToken = async (event) => {
+  // Decode authorization code extracted from the URL query
+  const code = decodeURIComponent(`${event.pathParameters.code}`);
+
+  return new Promise((resolve, reject) => {
+    oAuth2Client.getToken(code, (error, response) => {
+      if (error) {
+        return reject(error);
+      }
+      return resolve(response);
+    });
+  })
+    .then((results) => {
+      // Respond with OAuth token 
+      return {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true,
+        },
+        body: JSON.stringify(results),
+      };
+    })
+    .catch((error) => {
+      return {
+        statusCode: 500,
+        body: JSON.stringify(error),
+      };
+    });
+};
